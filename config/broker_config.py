3bc0aee0ff4e0f60f8b84602ed37c5a074b49900
Karl Braun <karltbraun@gmail.com>
2025-03-31 13:02:22 -0700

    Add new broker configurations for ts-vultr1 and n-vultr2 with default settings

--- config/broker_config.py
+++ config/broker_config.py
@@ -28,8 +28,22 @@ BROKER_CONFIG = {
         "MQTT_PASSWORD": "",
         "MQTT_KEEPALIVE": MQTT_DEFAULT_KEEPALIVE,
     },
-    "TS-VULTR1": {
-        "MQTT_BROKER_ADDRESS": "TS-Vultr1",
+    "ts-vultr1": {
+        "MQTT_BROKER_ADDRESS": "vultr1",
+        "MQTT_BROKER_PORT": MQTT_DEFAULT_PORT,
+        "MQTT_USERNAME": "",
+        "MQTT_PASSWORD": "",
+        "MQTT_KEEPALIVE": MQTT_DEFAULT_KEEPALIVE,
+    },
+    "n-vultr2": {
+        "MQTT_BROKER_ADDRESS": "n-vultr2",
+        "MQTT_BROKER_PORT": MQTT_DEFAULT_PORT,
+        "MQTT_USERNAME": "",
+        "MQTT_PASSWORD": "",
+        "MQTT_KEEPALIVE": MQTT_DEFAULT_KEEPALIVE,
+    },
+    "ts-vultr2": {
+        "MQTT_BROKER_ADDRESS": "vultr2",
         "MQTT_BROKER_PORT": MQTT_DEFAULT_PORT,
         "MQTT_USERNAME": "",
         "MQTT_PASSWORD": "",
